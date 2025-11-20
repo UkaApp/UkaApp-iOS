d623672e05faf06c989cfed5a12afbc673c9045c
kasper280403 <kasper.s.karlsen@gmail.com>
2025-11-20 19:33:21 +0100

    feat: Splash screen added

--- ukaAppen/splash.swift
+++ ukaAppen/splash.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SplashView: View {
+    @State private var shouldNavigate = false
+
+    var body: some View {
+        Group {
+            if shouldNavigate {
+                ContentView()
+            } else {
+                VStack {
+                    Text("Powered by:")
+                        .font(.largeTitle)
+                        .bold()
+                    Image("main_sponsor_img")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(hex: "#FF8D00"))
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            shouldNavigate = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
