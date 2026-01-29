4c88ef4316c635da7884a65918b274b3dc94e274
Marte Tangen <martetangen1@gmail.com>
2026-01-29 22:58:23 +0100

    refactor: split ContentView into seperate screens

---
+++ ukaAppen/Resources/Views/CountdownView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownView.swift
+//  ukaAppen
+//
+//  Created by Marte Tangen on 29/01/2026.
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownView()
+}
---
+++ ukaAppen/Resources/Views/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  ukaAppen
+//
+//  Created by Marte Tangen on 29/01/2026.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
