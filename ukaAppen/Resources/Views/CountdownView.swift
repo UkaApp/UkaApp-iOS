32c114e2da9a9eebfb4da2cc30aa23ca38628624
Marte Tangen <martetangen1@gmail.com>
2026-01-30 21:44:50 +0100

    refactor: Split ContentView and move code to CountdownView

--- ukaAppen/Resources/Views/CountdownView.swift
+++ ukaAppen/Resources/Views/CountdownView.swift
@@ -6,13 +6,73 @@
 //
 
 import SwiftUI
+import Combine
+
 
 struct CountdownView: View {
+    
+    //Choose date for countdown
+    let targetDate = Calendar.current.date(from: DateComponents(year: 2026, month: 10, day: 10, hour: 0, minute: 0, second: 0))!
+    
+    @State private var timeRemaining = ""
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color("UKAOrange")
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 40) {
+                    Text("UKA i Ås!")
+                        .font(.custom("Poppins-BoldItalic", size: 40))
+                        .foregroundColor(Color("NeutralCream"))
+                    
+                    Text(timeRemaining)
+                        .font(.custom("DMSans-Bold", size: 40))
+                        .monospacedDigit()
+                        .foregroundColor(Color("NeutralCream"))
+                        .onReceive(timer) { _ in
+                            updateCountdown()
+                        }
+                    
+                    NavigationLink {
+                        PushNotificationsView()
+                    } label: {
+                        Text("Trykk her")
+                            .font(.custom("DMSans-Bold", size: 30))
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color("NeutralCream"))
+                            .foregroundColor(Color("UKAOrange"))
+                            .cornerRadius(12)
+                            .padding(.horizontal)
+                    }
+                }
+                .padding()
+                .onAppear {
+                    updateCountdown()
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    CountdownView()
-}
+        func updateCountdown() {
+            let now = Date()
+            let diff = targetDate.timeIntervalSince(now)
+            
+            if diff > 0 {
+                let days = Int(diff) / 86400
+                let hours = (Int(diff) % 86400) / 3600
+                let minutes = (Int(diff) % 3600) / 60
+                let seconds = Int(diff) % 60
+                timeRemaining = String(format: "%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+            } else {
+                timeRemaining = "Endelig UKA!!!"
+            }
+        }
+    }
+    
+    #Preview {
+        CountdownView()
+    }
+    
